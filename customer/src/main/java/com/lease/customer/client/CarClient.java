a4a4e30cfcad7ab4543c90bf9ed91de933323208
DivGuru <divya.03g@gmail.com>
2024-10-01 14:27:43 -0700

    unit test added customer controller

--- customer/src/main/java/com/lease/customer/client/CarClient.java
+++ customer/src/main/java/com/lease/customer/client/CarClient.java
@@ -1,4 +1,23 @@
 package com.lease.customer.client;
 
-public class CarClient {
+import com.lease.customer.dto.CarDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@FeignClient(name="carModel",configuration = com.lease.customer.config.FeignClientConfig.class)
+public interface CarClient {
+
+    @GetMapping("/Car/{id}")
+    CarDTO getCarById(@PathVariable("id") Long id);
+
+    @PostMapping("/Car/add")
+    CarDTO addNewCarDetails(CarDTO carDTO);
+
+    @PutMapping("/Car/add")
+    CarDTO updateCarDetails(CarDTO carDTO);
+
+   // CarDTO deleteCar(Car)
 }
