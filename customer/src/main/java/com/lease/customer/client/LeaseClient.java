e54aae4c589fcd7063c53b6ca560a0c096da014a
DivGuru <divya.03g@gmail.com>
2024-10-03 12:10:14 -0700

    Added lease client and controller

--- customer/src/main/java/com/lease/customer/client/LeaseClient.java
+++ customer/src/main/java/com/lease/customer/client/LeaseClient.java
@@ -1,14 +1,25 @@
 package com.lease.customer.client;
 
 import com.lease.customer.dto.LeaseDTO;
+import com.lease.customer.dto.ReturnDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "leaseModel", configuration = com.lease.customer.config.FeignClientConfig.class)
 public interface LeaseClient {
 
-    @GetMapping("/lease/{id}")
-    ResponseEntity<LeaseDTO> getLease(@PathVariable("id") Long id);
+    @GetMapping("/{id}")
+    public ResponseEntity<LeaseDTO> getLease(@PathVariable Long id);
+
+    @PostMapping("/Submit")
+    public ResponseEntity<LeaseDTO> AddLease(@RequestBody LeaseDTO leaseDTO);
+
+    @PostMapping("/return/{id}")
+    public ResponseEntity<String> ReturnCar(@PathVariable Long id, @RequestBody ReturnDTO returnDto);
+    
 }
--- customer/src/main/java/com/lease/customer/controller/LeaseController.java
+++ customer/src/main/java/com/lease/customer/controller/LeaseController.java
@@ -1,9 +1,37 @@
 package com.lease.customer.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.lease.customer.client.LeaseClient;
+import com.lease.customer.dto.LeaseDTO;
+import com.lease.customer.dto.ReturnDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/lease")
 public class LeaseController {
-    
 
+    @Autowired
+    LeaseClient leaseClient;
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
+    public ResponseEntity<LeaseDTO> getLease(@PathVariable Long id) throws Exception {
+        return leaseClient.getLease(id);
+    }
+
+    @PostMapping("/Submit")
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
+    public ResponseEntity<LeaseDTO> AddLease(@RequestBody LeaseDTO leaseDTO) throws Exception {
+        System.out.println("Submitting lease details ");
+        return leaseClient.AddLease(leaseDTO);
+    }
+
+    @PostMapping("/return/{id}")
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
+    public ResponseEntity<String> ReturnCar(@PathVariable Long id, @RequestBody ReturnDTO returnDto) throws Exception{
+       // System.out.println(""returnDto.getReturnDistance());
+        return leaseClient.ReturnCar(id, returnDto);
+    }
 }
